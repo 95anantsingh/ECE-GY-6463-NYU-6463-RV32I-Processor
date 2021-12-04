4275a06f731eba2b4eb15df1a05ee4d217b5ed2c
EeshaJindal <ej2137@nyu.edu>
2021-12-04 00:44:03 -0500

    Update DataExt.v

--- Project_Vivado/Project_Vivado.srcs/sources_1/new/DataExt.v
+++ Project_Vivado/Project_Vivado.srcs/sources_1/new/DataExt.v
@@ -1,49 +1,45 @@
 `timescale 1ns / 1ps
+`include "defines.vh"
+`default_nettype none
 
 module DataExt(
-    input wire [2:0] opcode,
-    input wire [31:0] data_in,
-    output reg [31:0] data_out
+input wire [2:0] opcode,
+input wire [31:0] data,
+output wire [31:0] dout
+
     );
+    
+    reg [31:0] d_out;
     
     always@(*) begin
         case(opcode)
-            3'd0: begin       //LB
-                if (data_in[7]==1)
-                    data_out <= {24'b111111111111111111111111, data_in[7:0]};
+            `LB: begin
+                if (data[7]==1)
+                    d_out <= {24'b111111111111111111111111, data[7:0]};
                 else
-                    data_out <= {24'b000000000000000000000000, data_in[7:0]};
+                    d_out <= {24'b000000000000000000000000, data[7:0]};
             end
-            3'd1: begin       //LH
-                if (data_in[15]==1)
-                    data_out <= {16'b1111111111111111, data_in[15:0]};
+            `LH: begin 
+                if (data[15]==1)
+                    d_out <= {16'b1111111111111111, data[15:0]};
                 else
-                    data_out <= {16'b0000000000000000, data_in[15:0]};    
+                    d_out <= {16'b0000000000000000, data[15:0]};    
             end
-            3'd2: begin       //LW
-                data_out <= data_in;
+            `LW: begin       //LW
+                d_out <= data;
             end
-            3'd3: begin       //LBU
-                    data_out <= {24'b000000000000000000000000, data_in[7:0]};
+            `LBU: begin
+                    d_out <= {24'b000000000000000000000000, data[7:0]};
             end
-            3'd4: begin       //LHU
-                data_out <= {16'b0000000000000000, data_in[15:0]}; 
+            `LHU: begin  
+                d_out <= {16'b0000000000000000, data[15:0]}; 
             end
             default:
                 $display("Incorrect opcode");
         endcase
     end
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    assign dout = d_out;
     
 endmodule
-
 
