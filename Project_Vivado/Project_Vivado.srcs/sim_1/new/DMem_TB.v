a32cb9171c1a826391222c1f4cdbaa76d9fac676
Tanisha-M97 <85574227+Tanisha-M97@users.noreply.github.com>
2021-12-05 11:02:16 -0500

    Update DMem_TB.v

--- Project_Vivado/Project_Vivado.srcs/sim_1/new/DMem_TB.v
+++ Project_Vivado/Project_Vivado.srcs/sim_1/new/DMem_TB.v
@@ -12,7 +12,7 @@ reg rd;
 reg rst;//active low reset
 wire  [31:0] data;
 
-DMem DUT(.addr(addr), .data_in(data_in), .we(we), .clk(clk), .rd(rd), .rst(rst), .data(data));
+DMem DUT(.addr_in(addr), .data_in(data_in), .we(we), .clk(clk), .rd(rd), .data_out(data));
 // generate clock
 always // no sensitivity list, so it always executes
 forever begin
@@ -26,7 +26,6 @@ initial begin
         rst=1'b1;
         we=4'b0001;
         addr=32'h80000000;
-//        data_in=32'h01000000;
         #1;
         if(data!=32'h00000000) $fatal("The Data memory or RAM is writing, no read operation");
         #9;
@@ -36,7 +35,6 @@ initial begin
         rst=1'b1;
         we=4'b0010;
          addr=32'h80000004;
-//        data_in=32'h00010000;
         #1;
         if(data!=32'h00000001) $fatal("The Data memory or RAM is writing, no read operation");
         #9;
@@ -47,7 +45,6 @@ initial begin
         rst=1'b1;
         we=4'b0100;
         addr=32'h80000008;
-//        data_in=32'h00000100;
         #1;
          if(data!=32'h00000002) $fatal("The Data memory or RAM is writing, no read operation");
         #9;
@@ -56,7 +53,6 @@ initial begin
         rst=1'b1;
         we=4'b1000;
          addr=32'h8000000c;
-//        data_in=32'h00000001;
         #1;
         if(data!=32'h00000003) $fatal("The Data memory or RAM is writing, no read operation");
         #9;
@@ -69,9 +65,7 @@ initial begin
         addr=32'h80000004;
         //data_in=32'h01000000;
         #5;
-        if(data!=32'h00000003) $fatal("The Data memory or RAM is not reading properly");  //Reads for the previous address before the next clk cycle
-        #10;
-        if(data!=32'h00000001) $fatal("The Data memory or RAM is not reading properly");
+        if(data!=32'h00000101) $fatal("The Data memory or RAM is not reading properly");
         #10;
         
 
