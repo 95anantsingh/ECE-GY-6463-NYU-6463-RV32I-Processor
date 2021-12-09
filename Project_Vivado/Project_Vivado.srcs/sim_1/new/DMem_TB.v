96439b3f4543565886905cda0e8dbd1a069c2e8e
Tanisha-M97 <85574227+Tanisha-M97@users.noreply.github.com>
2021-12-09 16:49:45 -0500

    Update DMem_TB.v

--- Project_Vivado/Project_Vivado.srcs/sim_1/new/DMem_TB.v
+++ Project_Vivado/Project_Vivado.srcs/sim_1/new/DMem_TB.v
@@ -26,49 +26,120 @@ initial begin
         rst=1'b1;
         we=4'b0001;
         addr=32'h80000000;
+        #10;
+
+        
+        rd=1'b1;
+        rst=1'b1;
+        we=4'b0000;
+        addr=32'h80000000;
+        //data_in=32'h01000000;
         #1;
-        if(data!=32'h00000000) $fatal("The Data memory or RAM is writing, no read operation");
+        if(data!=32'h00000001) $fatal("The Data memory or RAM is not reading properly");
         #9;
+        
         
         
         rd=1'b0;
         rst=1'b1;
         we=4'b0010;
          addr=32'h80000004;
-        #1;
-        if(data!=32'h00000001) $fatal("The Data memory or RAM is writing, no read operation");
-        #9;
+        #10;
         
+        rd=1'b1;
+        rst=1'b1;
+        we=4'b0000;
+        addr=32'h80000004;
+        //data_in=32'h01000000;
+        #1;
+        if(data!=32'h00000100) $fatal("The Data memory or RAM is not reading properly");
+        #9;
         
         
         rd=1'b0;
         rst=1'b1;
         we=4'b0100;
         addr=32'h80000008;
-        #1;
-         if(data!=32'h00000002) $fatal("The Data memory or RAM is writing, no read operation");
-        #9;
+        #10;
 
+        rd=1'b1;
+        rst=1'b1;
+        we=4'b0000;
+        addr=32'h80000008;
+        //data_in=32'h01000000;
+        #1;
+        if(data!=32'h00010000) $fatal("The Data memory or RAM is not reading properly");
+        #9;
+        
         rd=1'b0;
         rst=1'b1;
         we=4'b1000;
          addr=32'h8000000c;
-        #1;
-        if(data!=32'h00000003) $fatal("The Data memory or RAM is writing, no read operation");
-        #9;
-
+        #10;
 
 
         rd=1'b1;
         rst=1'b1;
         we=4'b0000;
-        addr=32'h80000004;
+        addr=32'h8000000c;
         //data_in=32'h01000000;
-        #5;
-        if(data!=32'h00000101) $fatal("The Data memory or RAM is not reading properly");
-        #10;
+        #1;
+        if(data!=32'h01000000) $fatal("The Data memory or RAM is not reading properly");
+        #9;
         
-
+        rd=1'b0;
+        rst=1'b1;
+        we=4'b1111;
+        addr=32'h80000010;
+        #10;
+       
+        
+        rd=1'b1;
+        rst=1'b1;
+        we=4'b0000;
+        addr=32'h80000010;
+        //data_in=32'h01000000;
+        #1;
+        if(data!=32'h01010101) $fatal("The Data memory or RAM is not reading properly");
+        #9;
+        
+        
+        addr=32'h00100000;
+        we=4'b0000;
+        rd=1'b1;
+        #1;
+        if(data!=32'h13391993) $fatal("The Data memory or RAM is not reading properly");
+        #9;
+        
+        
+        addr=32'h00100004;
+        we=4'b0000;
+        rd=1'b1;
+        #1;
+        if(data!=32'h13643732) $fatal("The Data memory or RAM is not reading properly");
+        #9;
+        
+        addr=32'h00100008;
+        we=4'b0000;
+        rd=1'b1;
+        #1;
+        if(data!=32'h17456399) $fatal("The Data memory or RAM is not reading properly");
+        #9;
+        
+//        addr=32'h00100010;
+//        we=4'b0000;
+//        rd=1'b1;
+//        #1;
+//        if(data!=32'h13391993) $fatal("The Data memory or RAM is not reading properly");
+//        #9;
+        
+//        addr=32'h00100014;
+//        we=4'b0000;
+//        rd=1'b1;
+//        #1;
+//        if(data!=32'h13391993) $fatal("The Data memory or RAM is not reading properly");
+//        #9;
+        
         $display("All tests passed");
         $finish;
 end
